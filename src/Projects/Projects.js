2ab80e75564923e1b00100dd8324726682a4ff82
John Harrison <john.randolph.harrison@gmail.com>
2017-12-16 23:19:11 -0600

    working on project page, going to convert it to take a json, also gonna break project into its on component

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 import './Projects.css';
+import placeholder from '../History/florida.png'
 
 export default function Projects (props){
     return (
         <div className="projects">
             <div className="title"> My Projects </div>
             <div className="content">
-                <div className="description">
-                    Coming soon...
+
+                <div className="project">
+                    <div className="project-title">Project Title</div>
+                    <div className="project-preview">
+                        <img src={placeholder} className="project-preview-img" />
+                        <div className="project-preview-description">
+                            I worked on creating my own HTML5/Typescript game engine from scratch.                             I worked on creating my own HTML5/Typescript game engine from scratch.
+                            I worked on creating my own HTML5/Typescript game engine from scratch.
+                            I worked on creating my own HTML5/Typescript game engine from scratch.
+                            I worked on creating my own HTML5/Typescript game engine from scratch.
+<br/>
+I worked on creating my own HTML5/Typescript game engine from scratch.
+I worked on creating my own HTML5/Typescript game engine from scratch.
+<br/><br/>
+I worked on creating my own HTML5/Typescript game engine from scratch.
+
+                        </div>
+                    </div>
                 </div>
+
             </div>
         </div>
     );
