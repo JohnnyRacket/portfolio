5465590900989e72e3cb2d21c690fe69cfaa627a
John Harrison <john.randolph.harrison@gmail.com>
2017-12-17 19:20:19 -0600

    project cards styled to where I dont hate them

--- src/Project/Project.js
+++ src/Project/Project.js
@@ -5,9 +5,13 @@ import placeholder from '../History/florida.png'
 export default function Project (props){
     return (
         <div className="project">
-            <div className="project-title"> {props.title} </div>
+            <div className="project-img-wrapper">
+                <div style={{backgroundImage: 'url(' + props.image + ')'}} className="project-img"></div>
+            </div>
+            <div className="project-title-box">
+                <div className="project-title"> {props.title} </div>
+            </div>
             <div className="project-preview">
-                <img src={props.image} className="project-preview-img" />
                 <div className="project-preview-description"> {props.body} </div>
             </div>
         </div>
