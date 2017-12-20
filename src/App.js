195d5014526379fba304370e475123f6b83487ae
John Harrison <john.randolph.harrison@gmail.com>
2017-12-20 12:59:36 -0600

    header tweak on mobile, and hash routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { HashRouter, Route, Link } from 'react-router-dom'
 
 import './App.css';
 import NavBar from './NavBar/NavBar';
@@ -18,7 +18,7 @@ import posts from './Blog/posts.json';
 export default class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <div className="App">
           <div className="Container">
             <NavBar/>
@@ -37,7 +37,7 @@ export default class App extends Component {
             <Footer/>
           </div>
         </div>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
